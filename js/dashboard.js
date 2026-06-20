54db16faf74e50e280e0d0d0b12024f07b856248
Alialbarry <ali.sy.303.as@gmail.com>
2026-06-20 05:43:28 +0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,6 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000/api";
-const token = localStorage.getItem('token');
-
+const API_BASE_URL = "https://veteran-antibody-strep.ngrok-free.dev/api";const token = localStorage.getItem('token');
 if (!token) {
     window.location.href = 'login.html';
 }
